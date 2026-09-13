fc752520da2eefacf27490c09db8251cb91323ed
fotoprostudio2026-ops <fotoprostudio2026@gmail.com>
2026-09-13 15:01:54 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const C = "ecociclo-profissional-v11";
+const F = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", e => e.waitUntil(caches.open(C).then(c => c.addAll(F)).then(() => self.skipWaiting())));
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
