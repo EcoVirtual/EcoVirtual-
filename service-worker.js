38de5e583f3ecde53ee1117163ab82b8f17dfcfd
fotoprostudio2026-ops <fotoprostudio2026@gmail.com>
2026-09-13 15:00:14 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const C="ecociclo-profissional-v10";const F=["./","./index.html","./ecociclo-app.html","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
